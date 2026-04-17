588dda762b5681060e36dc007de1553efe109c7d
hhy <13612276628@163.com>
2026-04-17 21:00:13 +0800

    k

--- binarytree.C
+++ binarytree.C
@@ -9,7 +9,7 @@ public:
   int key = 0;
   T value = 0;
   
-  node():parent(NULL), left(NULL), right(NULL){}
+  node(int k, T value):parent(NULL), left(NULL), right(NULL), value(value), key(k){}
   
   void l(node* a){
     left = a;
@@ -22,25 +22,69 @@ public:
   void r(node* a){
     right = a;
   }
+  
+  bool all_empty(){
+    return left==NULL && right==NULL;
+  }
 
 };
+
 template < typename T >
 class BinaryTree{
   node<T>* root = NULL;
   
-  T get(int key, node<T>* sub){
+  T get_in(int key, node<T>* sub){ 
     if(sub == NULL)return NULL;
-    if(sub.key==key)return sub.value;
-    if(sub.left == NULL && sub.right == NULL)return NULL;
-    if(sub.left == NULL || key > sub.left.key)get(sub.right);
-    else if(key < sub.left.key)get(sub.left);
+    if(sub->key==key)return sub->value;
+    if(sub->all_empty())return NULL;
+    else if(key > sub->key)return get_in(key, sub->right);
+    else return get_in(key, sub->left);
   }
   
-  void insert(){
-    
+  void insert_in(node<T>* target, node<T>* sub){
+    if(is_empty()){root = target;return;}
+    if(sub->key > target->key){
+      if(sub->left != NULL)insert_in(target, sub->left);
+      else{
+        sub->l(target);
+        target->p(sub);
+      }
+    }else{
+      if(sub->right != NULL)insert_in(target, sub->right);
+      else{
+        sub->r(target);
+        target->p(sub);
+      }
+    }
   }
+  
+public:
+  BinaryTree(){}
+  
+  T get(int key){
+    if(is_empty())return NULL;
+    return get_in(key, root);
+  }
+  
+  void insert(int key, T value){
+    insert_in(new node<T>(key, value), root);
+  }
+  
+  bool is_empty(){
+    return root==NULL;
+  }
+  
+  
 };
 
 int main(){
-
+  BinaryTree<double> B;
+  B.insert(2, 10.5);
+  B.insert(5, 11.5);
+  B.insert(4, 12.5);
+  B.insert(1, 13.5);
+  B.insert(3, 14.5);
+  for(int i=1;i<=5;i++){
+    cout<<B.get(i)<<endl;
+  }
 }
